78d1067b4ec68a9b077b4c457f1a0a9dc9bacc09
Eric Meyer <emeyer@8thlight.com>
2011-04-25 12:20:01 -0500

    rvm bundler and vlad playing nicely together

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,12 +24,26 @@ namespace :vlad do
     run "rvm rvmrc trust #{release_path}"
   end
   
-  remote_task :bundle_install do
-    run "bundle install --without test"
+  remote_task :bundle do
+    # loads RVM, which initializes environment and paths
+    init_rvm = "source ~/.rvm/scripts/rvm"
+
+    # automatically trust the gemset in the .rvmrc file
+    trust_rvm = "rvm rvmrc trust #{release_path}"
+
+    # ya know, get to where we need to go
+    # ex. /var/www/my_app/releases/12345
+    goto_app_root = "cd #{release_path}"
+
+    # run bundle install with explicit path and without test dependencies
+    bundle_install = "bundle install $BUNDLE_PATH --without test"
+
+    # actually run all of the commands via ssh on the server
+    run "#{init_rvm} && #{trust_rvm} && #{goto_app_root} && #{bundle_install}"
   end
-  
+
   desc "Updates your application server to the latest revision, run the migrate rake task for the the app, then restarts Passenger"
-  remote_task :deploy => [:update, :rvm_trust, :bundle_install, :migrate, :start]
+  remote_task :deploy => [:update, :rvm_trust, :bundle, :migrate, :start]
   
 end
 
